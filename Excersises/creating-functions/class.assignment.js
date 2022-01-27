2c12f970d844a8a4df9b25fb22a6a6cdc577af28
Christian <christianlockley4@gmail.com>
2022-01-26 16:22:17 -0800

    completed the creating and using functions assignment

---
+++ Excersises/creating-functions/class.assignment.js
@@ -0,0 +1,9 @@
+/*create a function named greet, that takes in two parameters: firstname and lastname
+the output of greet should be a string ex.
+greet("kayla", "golder")
+Output: "hello, kayla golder. Its nice to meet you"*/
+
+function greet(firstname, lastname) {
+    return "Hello, " + firstname + lastname + ". It's nice to meet you."
+}
+console.log(greet("Kayla", " Golder"))
